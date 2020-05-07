92f5ce091c8020bba3bb1d1fe0a6eba342b0c81f
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-07 17:12:00 +0545

    Add tooltip with css

--- src/ui/tooltip.js
+++ src/ui/tooltip.js
@@ -1,3 +1,5 @@
+import './styles/tooltip.css'
+
 class Tooltip{
     constructor(element){
         this.element = element;
@@ -8,6 +10,13 @@ class Tooltip{
         tip.classList.add('tip');
         tip.textContent = this.message;
         this.element.appendChild(tip);
+
+        this.element.addEventListener('mouseenter', ()=>{
+            tip.classList.add('active');
+        });
+        this.element.addEventListener('mouseleave', ()=>{
+            tip.classList.remove('active');
+        });
     }
 }
 
