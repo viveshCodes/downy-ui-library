0831231da062f3b834a9d2d36a6689bf84a57a9d
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-07 15:41:58 +0545

    Add css-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,15 +15,21 @@ module.exports = {
     },
     // to run babel
     module:{
-        rules:[{
-            test:/\.js$/,
-            exclude:/node_modules/,
-            use:{
-                loader:'babel-loader',
-                options:{
-                   presets: ['@babel/preset-env']
-                }    
+        rules:[
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                    presets: ['@babel/preset-env']
+                    }    
+                }
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader', 'css-loader']  // it runs from right to left
             }
-        }]
-    }
+        ]
+    }// modules
 };  
